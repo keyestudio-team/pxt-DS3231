879445ad2ebc65ecac4c190ce95d8399ce52937f
mworkfun <39076163+mworkfun@users.noreply.github.com>
2020-11-11 09:31:40 +0800

    Fixed year error

--- main.ts
+++ main.ts
@@ -218,7 +218,7 @@ namespace DS3231 {
         buf[4] = decToBcd(day);
         buf[5] = decToBcd(date);
         buf[6] = decToBcd(month);
-        buf[7] = decToBcd(year);
+        buf[7] = (year -2000)/10*16 + (year -2000)%10;
         pins.i2cWriteBuffer(DS3231_I2C_ADDR, buf)
     }
     
